5564a37040649108449bd2f9b8db86e1d36e75e1
gulliish <gulmiraabdujalilova@gmail.com>
2022-09-24 20:30:53 +0600

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect} from "react";
 import './App.css';
+import Login from "./components/Content";
 import NavScrollExample from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <NavScrollExample/>
+      <Login/>
     </div>
   );
 }   
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function NavScrollExample() {
   return (
     <Navbar bg="dark" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand href="#">Authorization</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -19,8 +19,8 @@ function NavScrollExample() {
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
+            <Nav.Link href="#action2">Contact us</Nav.Link>
+            <NavDropdown title="Help" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
                 Another action
@@ -30,9 +30,6 @@ function NavScrollExample() {
                 Something else here
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
